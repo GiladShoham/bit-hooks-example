9d5b44daf53ef052b72a291a054881ee436ea410
Gilad Shoham <shoham.gilad@gmail.com>
2019-09-26 15:14:05 +0300

    update extension - register to pre export hook

--- extension-workspace/src/bit-hooks/index.js
+++ extension-workspace/src/bit-hooks/index.js
@@ -7,15 +7,24 @@ const hooks = {
   init: ({rawConfig, dynamicConfig, api}) => {
     logger = api.getLogger();
     api.registerActionToHook('pre-tag', { name: 'preTagHook', run: runPreTagHook});
+    api.registerActionToHook('pre-export', { name: 'preExportHook', run: runPreExportHook});
   }
 }
 
 const runPreTagHook = (args, headers) => {
+  _writeToLog('pre-tag', args);
+}
+
+const runPreExportHook = (args, headers) => {
+  _writeToLog('pre-export', args);
+}
+
+const _writeToLog = (hookName, args) => {
   // Do whatever you want here
   const filePath = path.join(process.cwd(), FILE_NAME);
   const data = `---------------------------------
 ${new Date(Date.now()).toLocaleString()}
-pre-tag was triggered with args
+${hookName} was triggered with args:
 ${JSON.stringify(args, null, 2)}
 ---------------------------------`
   if (!fs.existsSync(filePath)){
