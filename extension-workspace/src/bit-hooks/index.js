c8bcac97a61727ff433f3dcb9fb4f37a35e32c9b
Gilad Shoham <shoham.gilad@gmail.com>
2019-09-26 13:47:19 +0300

    fix extension

--- extension-workspace/src/bit-hooks/index.js
+++ extension-workspace/src/bit-hooks/index.js
@@ -6,20 +6,20 @@ const FILE_NAME = 'events.log';
 const hooks = {
   init: ({rawConfig, dynamicConfig, api}) => {
     logger = api.getLogger();
-    api.registerActionToHook('pre-tag', { name: 'preTagHook', run: runExportWebHook});
+    api.registerActionToHook('pre-tag', { name: 'preTagHook', run: runPreTagHook});
   }
 }
 
 const runPreTagHook = (args, headers) => {
   // Do whatever you want here
-  const filePath = path.join(process.cwd, FILE_NAME);
+  const filePath = path.join(process.cwd(), FILE_NAME);
   const data = `---------------------------------
-${new Date(Date.now()).toLocaleString();}
+${new Date(Date.now()).toLocaleString()}
 pre-tag was triggered with args
-${args}
+${JSON.stringify(args, null, 2)}
 ---------------------------------`
   if (!fs.existsSync(filePath)){
-    fs.writeFile(filePath, data);
+    fs.writeFileSync(filePath, data);
   }
   fs.appendFileSync(filePath, data);
 };
